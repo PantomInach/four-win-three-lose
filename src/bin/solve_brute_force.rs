81f8ba6a76ab0d1b99fdfc012b8c4fdcaf28ddd3
Aaron Neugebauer <aaron@ralf-saalmueller.de>
2024-01-14 02:00:04 +0100

    feat: switched to fasted solving method

--- src/bin/solve_brute_force.rs
+++ src/bin/solve_brute_force.rs
@@ -2,7 +2,6 @@ use four_win_three_lose::field::Field;
 
 fn main() {
     let mut field = Field::new();
-    let res =
-        field.brute_force_game_state(false, false, &Field::possible_moves_symmetrical_if_sparse);
-    println!("{:?}", res);
+    let res = field.brute_force_game_state(false, false, &Field::possible_non_symmetrical_moves);
+    println!("{}", res);
 }
